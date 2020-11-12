ff943877b796effbe9de8894f3168336bf155b14
apoorumihikatha-lkw <74354128+apoorumihikatha-lkw@users.noreply.github.com>
2020-11-12 16:04:50 +0530

    Update play.js

--- commands/play.js
+++ commands/play.js
@@ -23,7 +23,7 @@ module.exports = {
     const { channel } = message.member.voice;
 
     const serverQueue = message.client.queue.get(message.guild.id);
-    if (!channel) return message.reply("You need to join a voice channel first!").catch(console.error);
+    if (!channel) return message.reply("You need to join a voice channel first Homie!").catch(console.error);
     if (serverQueue && channel !== message.guild.me.voice.channel)
       return message.reply(`You must be in the same channel as ${message.client.user}`).catch(console.error);
 
@@ -107,7 +107,7 @@ module.exports = {
     if (serverQueue) {
       serverQueue.songs.push(song);
       return serverQueue.textChannel
-        .send(`✅ **${song.title}** has been added to the queue by ${message.author}`)
+        .send(`✅ **${song.title}** has been added to the queue by ${message.author} Homie`)
         .catch(console.error);
     }
 
