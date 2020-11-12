87f10172c0c86716200bcee987397583e8a8d2ca
apoorumihikatha-lkw <74354128+apoorumihikatha-lkw@users.noreply.github.com>
2020-11-12 16:03:23 +0530

    Update pause.js

--- commands/pause.js
+++ commands/pause.js
@@ -5,7 +5,7 @@ module.exports = {
   description: "Pause the currently playing music",
   execute(message) {
     const queue = message.client.queue.get(message.guild.id);
-    if (!queue) return message.reply("There is nothing playing.").catch(console.error);
+    if (!queue) return message.reply("There is nothing playing Homies.").catch(console.error);
     if (!canModifyQueue(message.member)) return;
 
     if (queue.playing) {
