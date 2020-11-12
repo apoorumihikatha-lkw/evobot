beffbc696f1157c99cc29c7d02c847000f97f56c
apoorumihikatha-lkw <74354128+apoorumihikatha-lkw@users.noreply.github.com>
2020-11-12 16:25:08 +0530

    Update EvobotUtil.js

--- util/EvobotUtil.js
+++ util/EvobotUtil.js
@@ -4,7 +4,7 @@ module.exports = {
     const botChannel = member.guild.voice.channelID;
 
     if (channelID !== botChannel) {
-      member.send("You need to join the voice channel first!").catch(console.error);
+      member.send("You need to join the voice channel first Homie!").catch(console.error);
       return;
     }
 
