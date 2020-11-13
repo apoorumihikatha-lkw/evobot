645581e2c45f330827103b702e5b5dbbaf6bc78f
apoorumihikatha-lkw <74354128+apoorumihikatha-lkw@users.noreply.github.com>
2020-11-13 16:42:40 +0530

    Update volume.js

--- commands/volume.js
+++ commands/volume.js
@@ -7,18 +7,18 @@ module.exports = {
   execute(message, args) {
     const queue = message.client.queue.get(message.guild.id);
 
-    if (!queue) return message.reply("There is nothing playing.").catch(console.error);
+    if (!queue) return message.reply("මුකුත් නෑ මිනිහො play කරන්න").catch(console.error);
     if (!canModifyQueue(message.member))
-      return message.reply("You need to join a voice channel first!").catch(console.error);
+      return message.reply("සින්දු දාන්න කලින් voice channel එකකට Join වෙයන් බල්ලො!").catch(console.error);
 
     if (!args[0]) return message.reply(`🔊 The current volume is: **${queue.volume}%**`).catch(console.error);
-    if (isNaN(args[0])) return message.reply("Please use a number to set volume.").catch(console.error);
+    if (isNaN(args[0])) return message.reply("Number එකක් දාපන් මිනිහො volume එක set කරන්න. පරයා!").catch(console.error);
     if (parseInt(args[0]) > 100 || parseInt(args[0]) < 0)
       return message.reply("Please use a number between 0 - 100.").catch(console.error);
 
     queue.volume = args[0];
     queue.connection.dispatcher.setVolumeLogarithmic(args[0] / 100);
 
-    return queue.textChannel.send(`Volume set to: **${args[0]}%**`).catch(console.error);
+    return queue.textChannel.send(`ඇද්ද තොට මූසලයා: **${args[0]}%**`).catch(console.error);
   }
 };
