39768a9e856abd0e4fe69e1a1d2ab8430445cfbb
apoorumihikatha-lkw <74354128+apoorumihikatha-lkw@users.noreply.github.com>
2020-11-13 16:45:42 +0530

    Update stop.js

--- commands/stop.js
+++ commands/stop.js
@@ -6,11 +6,11 @@ module.exports = {
   execute(message) {
     const queue = message.client.queue.get(message.guild.id);
 
-    if (!queue) return message.reply("There is nothing playing.").catch(console.error);
+    if (!queue) return message.reply("Stop කරන්න මුකුත් නෑ බල්ලො.").catch(console.error);
     if (!canModifyQueue(message.member)) return;
 
     queue.songs = [];
     queue.connection.dispatcher.end();
-    queue.textChannel.send(`${message.author} ⏹ stopped the music!`).catch(console.error);
+    queue.textChannel.send(`${message.author} ⏹ යනව තමයි බල්ලො, ගල් මූසලයා!`).catch(console.error);
   }
 };
